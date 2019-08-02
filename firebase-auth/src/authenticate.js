2086881b626deea61522cca5f2ba2fd66addf2a5
Calvin Mann <calvinmann@gmail.com>
2019-08-02 22:44:21 +0200

    Changed authenticate to recognize input structure

--- firebase-auth/src/authenticate.js
+++ firebase-auth/src/authenticate.js
@@ -5,20 +5,23 @@ module.exports = function createAuthenticateHandler(rest) {
      * Searches for a record using the key fields from the given input
      * @param {*} source Unused
      * @param {object} args The arguments to refresh the id token with
-     * @param {object} args.email The user's email
-     * @param {object} args.password The user's password
-     * @param {object} args.token A token to use to login
+     * @param {object} args.input.email The user's email
+     * @param {object} args.input.password The user's password
+     * @param {object} args.input.token A token to use to login
      */
     async function authenticate(source, args) {
+        if (!args.input) {
+            throw new Error(`input MUST be supplied`);
+        }
         let tokenData;
-        if (typeof args.email === `string` && typeof args.password === `string`) {
-            tokenData = await rest.verifyPassword(args.email, args.password);
+        if (typeof args.input.email === `string` && typeof args.input.password === `string`) {
+            tokenData = await rest.verifyPassword(args.input.email, args.input.password);
             if (tokenData) {
                 tokenData.userId = tokenData.localId;
                 delete tokenData.localId;
             }
-        } else if (typeof args.token === `string`) {
-            tokenData = await rest.verifyCustomToken(args.token);
+        } else if (typeof args.input.token === `string`) {
+            tokenData = await rest.verifyCustomToken(args.input.token);
         } else {
             throw new Error(`Either email and password must be supplied or "customToken"`);
         }
